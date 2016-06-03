550717ca584c33fb35ecaaccaed42693f794e2a9
Noah Halford <noah.halford@gmail.com>
2016-06-03 04:01:20 -0500

    hough.py broken

--- src/python/hough.py
+++ src/python/hough.py
@@ -3,69 +3,120 @@
 import cv2
 import numpy as np
 
-path = './test_scan/test.jpg'
-threshold = 150
+path = './test_scan/video_test.mov'
+threshold = 100
 
-img = cv2.imread(path)
+vidcap = cv2.VideoCapture(path)
+
+success, img = vidcap.read()
+
+while success:
+
+#img = cv2.imread(path)
 # NumPy array for threshold image
-result = np.zeros(np.shape(img))
+    result = np.zeros(np.shape(img))
+    
+    y, x = np.shape(img[:,:,1])
+    
+    thresh = cv2.inRange(img,np.array([0,0,threshold]),np.array([255,255,255]))
+    cv2.imwrite("threshold.png", thresh)
+    
+    nonzero = np.nonzero(thresh)
+    
+    # White pixels, each entry is an array [x y]
+    nonzero_coords = np.zeros((np.shape(nonzero[0])[0], 2))
+    nonzero_coords[:,0] = nonzero[1]
+    nonzero_coords[:,1] = nonzero[0]
+    
+    result = cv2.cvtColor(thresh,cv2.COLOR_GRAY2RGB)
+    
+    # Source: http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html
+    lines = cv2.HoughLines(np.array(threshold, dtype=np.uint8),0.1,np.pi/60,20)
+    print lines
+    if np.any(lines):
+        coords = lines[0] # (rho, theta)
+        th_max = np.amax(coords.T[1])
+        filtered_list = filter(lambda x: x != 0, (coords.T[1]).tolist())
+        if len(filtered_list) > 0:
+            th_min = min(filtered_list)
+            min_found = False
+            max_found = False
 
-y, x = np.shape(img[:,:,1])
-print x
-print y
-
-thresh = cv2.inRange(img,np.array([0,0,threshold]),np.array([255,255,255]))
-cv2.imwrite("threshold.png", thresh)
-
-eroded = cv2.erode(thresh, np.ones((5,5)))
-cv2.imwrite("eroded.png", thresh)
-
-result = cv2.cvtColor(thresh,cv2.COLOR_GRAY2RGB)
-
-# Source: http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html
-lines = cv2.HoughLines(np.array(eroded, dtype=np.uint8),0.1,np.pi/60,50)
-coords = lines[0] # (rho, theta)
-th_max = np.amax(coords.T[1])
-th_min = min(filter(lambda x: x != 0, (coords.T[1]).tolist()))
-min_found = False
-max_found = False
-
-axes = np.zeros((2,2))
-
-for rho,theta in coords:
-    if (theta == th_min and not min_found) or (theta == th_max and not max_found):
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a*rho
-        y0 = b*rho
-        x1 = int(x0 + x*(-b))
-        y1 = int(y0 + x*(a))
-        x2 = int(x0 - y*(-b))
-        y2 = int(y0 - y*(a))
-
-        slope = np.tan(1 / theta)
-        intercept = rho / np.sin(theta)
-
-        if theta == th_min:
-            min_found = True
-            axes[0] = np.array([slope, intercept])
-        else:
-            max_found = True
-            axes[1] = np.array([slope, intercept])
-
-        cv2.line(result,(x1,y1),(x2,y2),(0,0,255),2)
-
-# Slope and intercepts
-m1, b1 = axes[0]
-m2, b2 = axes[1]
-
-def unit(slope):
-    theta = np.arctan(1/slope)
-    return (np.cos(theta), np.sin(theta))
-
-x_intersection = (b2 - b1) / (m1 - m2)
-y_intersection = m1 * x_intersection + b1
-
-w = np.sqrt(x_intersection**2 + (y_intersection - y/2)**2)
-
-cv2.imwrite('hough.png',result)
+            print "Min: {}".format(th_min)
+            print "Max: {}".format(th_max)
+            
+            axes = np.zeros((2,2))
+            
+            for rho,theta in coords:
+                if (theta == th_min and not min_found) or (theta == th_max and not max_found):
+                    a = np.cos(theta)
+                    b = np.sin(theta)
+                    x0 = a*rho
+                    y0 = b*rho
+                    x1 = int(x0 + x*(-b))
+                    y1 = int(y0 + x*(a))
+                    x2 = int(x0 - y*(-b))
+                    y2 = int(y0 - y*(a))
+            
+                    slope = -a / b
+                    intercept = rho / b
+            
+                    if theta == th_min:
+                        min_found = True
+                        axes[0] = np.array([slope, intercept])
+                    else:
+                        max_found = True
+                        axes[1] = np.array([slope, intercept])
+            
+                    cv2.line(result,(x1,y1),(x2,y2),(0,0,255),2)
+            
+            cv2.imwrite('hough.png',result)
+            
+            # Slope and intercepts
+            m1, b1 = axes[0]
+            m2, b2 = axes[1]
+            
+            def unit(slope):
+                theta = np.arctan(1/slope)
+                return np.array([np.cos(theta), np.sin(theta)])
+            
+            x_intersection = (b2 - b1) / (m1 - m2)
+            y_intersection = m1 * x_intersection + b1
+            
+            w = np.sqrt(x_intersection**2 + (y_intersection - y/2)**2)
+            
+            # All nonzero points, indexed by y value
+            pointList = {}
+            for i, j in nonzero_coords:
+                if j in pointList.keys():
+                    pointList[j].append(i)
+                else:
+                    pointList[j] = [i]
+            
+            # The points that we will use in our output, indexed by y
+            # value
+            points = np.zeros((len(pointList.keys()), 2))
+            i = 0
+            for y in pointList:
+                x = np.median(np.array(pointList[y]))
+                points[i] = np.array([x,y])
+                i += 1
+            
+            u = unit(m1)
+            v = unit(m2)
+            
+            u_matrix = np.column_stack((u,v))
+            
+            dot_prods = np.dot(points, u_matrix)
+            
+            w_col = w*np.ones(i)
+            
+            # Each entry will be a point in our point cloud
+            point_matrix = np.column_stack((dot_prods, w_col))
+            
+            f = open('out.xyz','w+')
+            for row in point_matrix:
+                f.write('{} {} {}\n'.format(row[0], row[1], row[2]))
+            f.close()
+    
+    success, img = vidcap.read()
